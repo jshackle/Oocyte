804509ef19cda81c932fc02eb10a2a889734198f
jshackle <41973998+jshackle@users.noreply.github.com>
2019-05-11 01:40:10 -0400

    Update masterData.m

--- masterData.m
+++ masterData.m
@@ -4,7 +4,6 @@ info = imfinfo(fname);
 num_images = numel(info);
 dataArray = cell(1, num_images);
 for kk = 1:num_images
-    tic;
     fname = "/home/john/Documents/BiophysicsUROP2/MAX_Actin_All.tif";
     info = imfinfo(fname);
     A = imread(fname, kk, 'Info', info);
@@ -14,8 +13,6 @@ for kk = 1:num_images
     B = double(B);
     B(B == 255) = 1;
     data = controlData(B, A);
-    %writematrix(data, ['FinalData/' ['data_' int2str(kk)] ]);
     dataArray{kk} = data;
     disp(kk);
-    toc;
 end
