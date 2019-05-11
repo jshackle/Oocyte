89d9ee85211761a7271222632554f47eea70d2e9
jshackle <41973998+jshackle@users.noreply.github.com>
2019-05-11 01:55:20 -0400

    Delete findNeighbors.m

--- findNeighbors.m
+++
@@ -1,7 +0,0 @@
-%Will return with list of points that are neighbors to the point in the
-%image with a max distance search of max ie will check at most max distance
-%away
-function [data] = findNeighbors(imageData, point, range)
-Data2 = findNeighborsHelper(imageData, point, range);
-data = find(Data2 == imageData == 0).';
-end
