64657bc90c35dc6d816ebf23be63fadd5e3a8262
jshackle <41973998+jshackle@users.noreply.github.com>
2019-05-11 01:45:24 -0400

    Update checkBoundary.m

--- checkBoundary.m
+++ checkBoundary.m
@@ -1,3 +1,4 @@
+%Checks two boundary images and returns true if they are similar (with regards to number of pixels)
 function [similar] = checkBoundary(prevBoundary, boundary)
 count1 = size(find(prevBoundary == 1), 1);
 count2 = size(find(boundary == 1), 1);
